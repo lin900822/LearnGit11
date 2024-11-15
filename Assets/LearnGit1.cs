4fedd465f7238f824d85d3096b857ff04d1e76bd
lin <wilson24386433@gmail.com>
2024-11-16 01:23:50 +0800

    這是第二次提交 新增了LearnGit1.cs腳本

---
+++ Assets/LearnGit1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnGit1 : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("這是LearnGit1 用指令的方式紀錄");
+    }
+}
